13b4437ad0e2937e3ed0e7f24e0685c5ce676953
KeysiCossio <112028178+KeysiCossio@users.noreply.github.com>
2022-08-24 19:39:48 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/*******************************************************************************************
+* Java Program Name
+********************************************************************************************
+* Keysi Cossio
+* 08/24/22
+* CMSC255 004
+*******************************************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("<Your Name>");
+		System.out.println("<Java Program Name>");
+		System.out.println("<Program/Project Description>");
+		System.out.println("<Date Created>");
+		System.out.println("<Course Number and Section Number>");
+		System.out.println();
+
+	}
+}
